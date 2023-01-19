49385083fcab8317486949e2a688659da47550e9
João Maia <joaocarlosbgm@gmail.com>
2023-01-19 10:06:44 -0300

    feat: context + provider + fix: factoring form

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
